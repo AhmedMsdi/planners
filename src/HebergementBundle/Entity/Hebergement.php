74ee3f4979aff7dbb7c4d43e5b423c60db00e369
Mahdi <mahdi.znaidi@esprit.tn>
2018-03-22 12:56:21 +0100

    lastOne

---
+++ src/HebergementBundle/Entity/Hebergement.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace HebergementBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Hebergement
+ *
+ * @ORM\Table(name="hebergement")
+ * @ORM\Entity
+ */
+class Hebergement
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="categorie", type="string", length=255, nullable=false)
+     */
+    private $categorie;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lieu", type="string", length=255, nullable=false)
+     */
+    private $lieu;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255, nullable=false)
+     */
+    private $description;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="prix", type="float", precision=10, scale=0, nullable=false)
+     */
+    private $prix;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="tel", type="integer", nullable=false)
+     */
+    private $tel;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="site_web", type="string", length=255, nullable=false)
+     */
+    private $siteWeb;
+
+
+}
+
